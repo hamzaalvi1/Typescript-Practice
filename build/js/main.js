f55bfd7b0cd50d202cb0f1fa9adfcb31ac8507f1
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-22 12:27:16 +0500

    Basic types complete

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("HELLO WORLD");
+// Basic Types
+// primitive types
+// string
+// number
+// boolean
+// null
+// undefined
+// Union types
+let userName = "Hamza Alvi";
+let dob = 15071997;
+let isPaid = false;
+let isUser = null;
+isUser = "Hamza Alvi";
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("HELLO WORLD");
+// Basic Types
+
+// primitive types
+
+// string
+// number
+// boolean
+// null
+// undefined
+// Union types
+let userName: string = "Hamza Alvi";
+let dob: number = 15071997;
+let isPaid: boolean = false;
+let isUser: null | string = null;
+isUser = "Hamza Alvi";
