fad7a7c2452a2fdc42248da863d0ffcb98c72dc2
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-22 12:02:30 +0500

    project struture developed

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log("HELLO WORLD");
-var greetings = function (userName) {
-    console.log("Hello ".concat(userName));
-};
-var sum = function (num1, num2) { return num1 + num2; };
-greetings("Hamza Alvi");
-console.log(sum(5, 6));
