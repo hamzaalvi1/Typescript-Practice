ca6e0ab86f9f0199f0a94ec0cdd5990d4b814924
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-22 13:23:56 +0500

    non primitive types WIP

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,18 @@
 "use strict";
-// Basic Types
-// primitive types
-// string
-// number
-// boolean
-// null
-// undefined
-// Union types
-let userName = "Hamza Alvi";
-let dob = 15071997;
-let isPaid = false;
-let isUser = null;
-isUser = "Hamza Alvi";
+console.log("Hey there!");
+const numbers = [3, 5, 6, 7, 8];
+const names = ["hamza", "Ameen", "yazib", "taha", "talha"];
+const isActives = [
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+];
+const phnNumbers = ["03413108435", 923413108435];
+console.log(phnNumbers);
+const newTuple = ["hamza", 26, true];
+const calculations = [43, (num1, num2) => num1 + num2];
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
-// Basic Types
+console.log("Hey there!");
+// non Primitive types
+// Arrays
+// Objects
+// functions
+// Symbols
 
-// primitive types
+type phoneNumber = string | number;
 
-// string
-// number
-// boolean
-// null
-// undefined
-// Union types
-let userName: string = "Hamza Alvi";
-let dob: number = 15071997;
-let isPaid: boolean = false;
-let isUser: null | string = null;
-isUser = "Hamza Alvi";
+type sum = (num1: number, num2: number) => number;
+
+type myTuple = [string, number, boolean];
+
+type sumTuple = [number, sum];
+
+const numbers: number[] = [3, 5, 6, 7, 8];
+const names: string[] = ["hamza", "Ameen", "yazib", "taha", "talha"];
+const isActives: boolean[] = [
+  true,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+];
+
+const phnNumbers: phoneNumber[] = ["03413108435", 923413108435];
+console.log(phnNumbers);
+
+const newTuple: myTuple = ["hamza", 26, true];
+const calculations: sumTuple = [43, (num1, num2) => num1 + num2];
