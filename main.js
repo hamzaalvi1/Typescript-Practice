c7d7263898bc650d80236595bcc0232863f006c3
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-22 11:43:06 +0500

    ts basics

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("HELLO WORLD");
+var greetings = function (userName) {
+    console.log("Hello ".concat(userName));
+};
+var sum = function (num1, num2) { return num1 + num2; };
+greetings("Hamza Alvi");
+console.log(sum(5, 6));
---
+++ main.ts
@@ -0,0 +1,11 @@
+console.log("HELLO WORLD");
+
+const greetings = (userName) => {
+  console.log(`Hello ${userName}`);
+};
+
+const sum = (num1, num2) => num1 + num2;
+
+greetings("Hamza Alvi");
+
+console.log(sum(5,6))
