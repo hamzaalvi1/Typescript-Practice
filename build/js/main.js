d4b289ce5026f1f179395d4133ebe8f82ea19899
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-22 21:50:00 +0500

    Interfaces done

--- build/js/main.js
+++ build/js/main.js
@@ -1,35 +1,20 @@
 "use strict";
-console.log("Hey there!");
-const numbers = [3, 5, 6, 7, 8];
-const names = ["hamza", "Ameen", "yazib", "taha", "talha"];
-const isActives = [
-    true,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-];
-const phnNumbers = ["03413108435", 923413108435];
-console.log(phnNumbers);
-const newTuple = ["hamza", 26, true];
-const calculations = [43, (num1, num2) => num1 + num2];
-let user1 = {
-    _id: "15091997",
-    name: "John Doe",
-    email: "john@doemailinator.com",
-    phnNumber: "+9231111111111",
-    address: "some address",
-    isGraduated: true,
-    degreeName: "Bachelor in Computer Science",
-    division: 3.5,
+console.log("Interface");
+const employee = {
+    name: "Hamza Alvi",
+    email: "hamza.ahmed@koderlabs.com",
+    designation: "Senior Software Engineer",
+    role: "employee",
 };
-user1.name = "Jane Doe";
-const getStudent = (student) => `${student.name}`;
-console.log(getStudent({ name: "Hamza Alvi" }));
-const showError = (message) => {
-    throw new Error(message);
+const myCat = {
+    ownerName: "Hamza Alvi",
+    name: "dodo",
+    species: "cat",
+    color: "tuxedo",
+    age: 2,
+    favoritesFood: "fish",
+    roar: function (sound) {
+        return `My ${this.species} ${this.name} says ${sound}`;
+    },
 };
-showError("bad request");
+console.log(myCat.roar("meow meow!"));
--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,55 @@
-console.log("Hey there!");
-// non Primitive types
-// Arrays
-// Objects
-// functions
-// Symbols
+console.log("Interface");
 
-type phoneNumber = string | number;
-
-type sum = (num1: number, num2: number) => number;
-
-type myTuple = [string, number, boolean];
-
-type sumTuple = [number, sum];
-
-const numbers: number[] = [3, 5, 6, 7, 8];
-const names: string[] = ["hamza", "Ameen", "yazib", "taha", "talha"];
-const isActives: boolean[] = [
-  true,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-];
-
-const phnNumbers: phoneNumber[] = ["03413108435", 923413108435];
-console.log(phnNumbers);
-
-const newTuple: myTuple = ["hamza", 26, true];
-const calculations: sumTuple = [43, (num1, num2) => num1 + num2];
-
-// objects
-
-type User = {
-  readonly _id: string;
+// custom type
+type Role = "admin" | "employee" | "line-manager";
+type Employee = {
   name: string;
   email: string;
-  phnNumber: number | string;
-  address: string;
-  isEmailVerified?: boolean;
-  hobbies?: string[];
+  designation: string;
+  role: Role;
 };
 
-type Qualification = {
-  isGraduated: boolean;
-  degreeName: string;
-  division: number | string;
+const employee: Employee = {
+  name: "Hamza Alvi",
+  email: "hamza.ahmed@koderlabs.com",
+  designation: "Senior Software Engineer",
+  role: "employee",
 };
 
-let user1: User & Qualification = {
-  _id: "15091997",
-  name: "John Doe",
-  email: "john@doemailinator.com",
-  phnNumber: "+9231111111111",
-  address: "some address",
-  isGraduated: true,
-  degreeName: "Bachelor in Computer Science",
-  division: 3.5,
+interface Animal {
+  name: string;
+  species: string;
+  color: string;
+  age?: number;
+  roar(sound: string): string;
+}
+
+interface Owner extends Animal {
+  ownerName: string;
+}
+
+interface Animal {
+  favoritesFood: string;
+}
+
+interface Vehicle {
+  color: string;
+  modelName: string;
+  modelNumber: string;
+  vehicleType: string;
+  year: number;
+}
+
+const myCat: Owner = {
+  ownerName: "Hamza Alvi",
+  name: "dodo",
+  species: "cat",
+  color: "tuxedo",
+  age: 2,
+  favoritesFood: "fish",
+  roar: function (sound: string) {
+    return `My ${this.species} ${this.name} says ${sound}`;
+  },
 };
-user1.name = "Jane Doe";
 
-// functions
-type Student = { name: string; rollNo?: number; batch?: number };
-type studentFunc = (student: Student) => string;
-
-const getStudent: studentFunc = (student: Student) => `${student.name}`;
-console.log(getStudent({ name: "Hamza Alvi" }));
-
-const showError = (message: string): never => {
-  throw new Error(message);
-};
-
-showError("bad request");
+console.log(myCat.roar("meow meow!"));
