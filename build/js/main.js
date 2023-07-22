a3574bafdde86d069218e068bffbfb7b49dc2c2c
hamza.ahmed@koderlabs.com <hamzaalvi224@gmail.com>
2023-07-22 19:03:17 +0500

    reference type complete

--- build/js/main.js
+++ build/js/main.js
@@ -16,3 +16,20 @@ const phnNumbers = ["03413108435", 923413108435];
 console.log(phnNumbers);
 const newTuple = ["hamza", 26, true];
 const calculations = [43, (num1, num2) => num1 + num2];
+let user1 = {
+    _id: "15091997",
+    name: "John Doe",
+    email: "john@doemailinator.com",
+    phnNumber: "+9231111111111",
+    address: "some address",
+    isGraduated: true,
+    degreeName: "Bachelor in Computer Science",
+    division: 3.5,
+};
+user1.name = "Jane Doe";
+const getStudent = (student) => `${student.name}`;
+console.log(getStudent({ name: "Hamza Alvi" }));
+const showError = (message) => {
+    throw new Error(message);
+};
+showError("bad request");
--- src/main.ts
+++ src/main.ts
@@ -31,3 +31,46 @@ console.log(phnNumbers);
 
 const newTuple: myTuple = ["hamza", 26, true];
 const calculations: sumTuple = [43, (num1, num2) => num1 + num2];
+
+// objects
+
+type User = {
+  readonly _id: string;
+  name: string;
+  email: string;
+  phnNumber: number | string;
+  address: string;
+  isEmailVerified?: boolean;
+  hobbies?: string[];
+};
+
+type Qualification = {
+  isGraduated: boolean;
+  degreeName: string;
+  division: number | string;
+};
+
+let user1: User & Qualification = {
+  _id: "15091997",
+  name: "John Doe",
+  email: "john@doemailinator.com",
+  phnNumber: "+9231111111111",
+  address: "some address",
+  isGraduated: true,
+  degreeName: "Bachelor in Computer Science",
+  division: 3.5,
+};
+user1.name = "Jane Doe";
+
+// functions
+type Student = { name: string; rollNo?: number; batch?: number };
+type studentFunc = (student: Student) => string;
+
+const getStudent: studentFunc = (student: Student) => `${student.name}`;
+console.log(getStudent({ name: "Hamza Alvi" }));
+
+const showError = (message: string): never => {
+  throw new Error(message);
+};
+
+showError("bad request");
